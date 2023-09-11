9986f88c73cd50acad312ac2b5c8f9476d5bc8a6
noval assalam <naufalassalam@gmail.com>
2023-09-11 20:36:38 +0700

    hapuss

--- hallo.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> //kode untuk input output dalam C++
-using namespace std; // library untuk memperingkas 
-
-int main() { //blok kode utama yang akan di run bentuknya int
-    std::cout << "Hello, World!" << std::endl; //asli
-    cout << "Hello, World!" << endl; //pakai library
-    return 0; //makanya di return 0
-}
