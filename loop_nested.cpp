cd8a6e1d3437772f44de1a72df949d59d2b6d0d5
noval assalam <naufalassalam@gmail.com>
2023-10-24 08:28:24 +0700

    nested loop

--- loop_nested.cpp
+++ loop_nested.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
     // Loop pertama (luar)
     for (int i = 1; i <= 3; i++) {
+        cout<<i<<endl;
+    }
+
+     for (int i = 1; i <= 3; i++) {
         // Loop kedua (dalam)
-        for (int j = 1; j <= 3; j++) {
+        for (int j = 1; j <= 2; j++) {
+
+
+             for (int k = 1; k <= 4; k++) {
             // Menampilkan hasil perkalian i dan j
-            cout << i << " * " << j << " = " << i * j << endl;
+            cout << i << " * " << k << " = " << i * k << endl;
+        }
+            // Menampilkan hasil perkalian i dan j
+            // cout << i << " * " << j << " = " << i * j << endl;
         }
     }
 
